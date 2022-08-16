0dcd61558e70aa955dd33f3e23719e323da06bce
Fidan Hoxha <fidanihoxha@gmail.com>
2022-08-16 10:15:28 +0200

    Created Game Over View and added background to all views

---
+++ ColorMatch/EndViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EndViewController.swift
+//  ColorMatch
+//
+//  Created by Fidan Hoxha on 16.8.22.
+//
+
+import UIKit
+
+class EndViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
