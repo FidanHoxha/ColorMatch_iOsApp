6e1e9574d4ec73f1916d3f90c6bb7fa8246b8823
Fidan Hoxha <69246069+FidanHoxha@users.noreply.github.com>
2022-08-21 15:42:02 +0200

    Added a descriptive comment to ScoreTableItem extension

--- ColorMatch/ScoreTableItem/ScoreTableItem+CoreDataProperties.swift
+++ ColorMatch/ScoreTableItem/ScoreTableItem+CoreDataProperties.swift
@@ -1,13 +1,15 @@
 import Foundation
 import CoreData
 
-
+// An extension of our ScoreTableItem class
 extension ScoreTableItem {
 
+    // A fetch request through which we query our Score List Items
     @nonobjc public class func fetchRequest() -> NSFetchRequest<ScoreTableItem> {
         return NSFetchRequest<ScoreTableItem>(entityName: "ScoreTableItem")
     }
 
+    // Properties saved in Core Data
     @NSManaged public var score: String?
     @NSManaged public var createdAt: Date?
 
